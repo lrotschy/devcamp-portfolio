962b7089f54a7a68a614bb6308d9808ba0e0ab31
Liela Rotschy <lielarotschy@gmail.com>
2020-08-26 16:33:58 -0600

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n| 
+  Blog.create(
+    title: "Blog Post #{n}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+      culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |n| 
+  Skill.create(
+    title: "Skill #{n}",
+    percent_utilized: 15
+  )
+end 
+
+puts "5 skills created"
+
+9.times do |n|
+  Portfolio.create(
+    title: "Portfolio #{n}",
+    subtitle: "My great service",
+    body: "Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut 
+      labore et dolore magna aliqua.",
+    main_image: "https://place-hold.it/600X400",
+    thumb_image: "https://place-hold.it/300x500"
+  )
+end 
+
+  puts "9 portfolio items created"
