0d025b7dfd22e87d79d92c82e8b799b0ee4863dc
Liela Rotschy <lielarotschy@gmail.com>
2020-08-27 15:38:15 -0600

    create edit and update function for portfolio items

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -40,6 +40,8 @@ class BlogsController < ApplicationController
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
+    pp "BLOG:"
+    pp @blog
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,47 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all 
+  end
+
+  def new 
+    @portfolio = Portfolio.new
+  end 
+
+  def show
+  end
+
+  def create 
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created!" }
+      else  
+        format.html { render :new }
+      end 
+    end 
+  end
+  
+  def edit 
+    # @portfolio_item = Portfolio.find(params[:id])
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update 
+    @portfolio = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private 
+
+  def portfolio_params 
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
 end
